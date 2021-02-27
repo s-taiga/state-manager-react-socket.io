ceed45d485c2fa149f2a7a826d04c351713e00d4
t-shibata <neverbefore351@gmail.com>
2021-02-27 16:45:50 +0900

    useReduceの処理修正

--- state-manage-react/src/App.tsx
+++ state-manage-react/src/App.tsx
@@ -6,36 +6,68 @@ import * as SIM from './socket_io_messages';
 import './App.css';
 
 // サーバーのアドレス
-const socket = socketIOClient('http://192.168.11.4:4001');
+const socket = socketIOClient('http://localhost:4001');
+
+interface ResourceAction {
+  type: string;
+  data: Resource[];
+}
+
+const resourceReducer: React.Reducer<Resource[], ResourceAction> = (currentResource: Resource[], action: ResourceAction) => {
+  if (action.type === "add") {
+    return [...currentResource, ...action.data];
+  } else if (action.type === "change") {
+    return currentResource.map(each_resource => {
+      const filteredResource: Resource[] = action.data.filter(each_data => each_data.id === each_resource.id);
+      if (filteredResource.length === 0) {
+        return each_resource;
+      } else {
+        return filteredResource[0];
+      }
+    });
+  } else if (action.type === "remove") {
+    return currentResource.filter(each_resource => action.data.filter(each_data => each_data.id === each_resource.id).length === 0);
+  } else if (action.type === "init") {
+    return action.data;
+  } else {
+    return [];
+  }
+}
+
+interface ResourceStateAction {
+  type: string;
+  data: ResourceState[];
+}
+
+const resourceStateReducer: React.Reducer<ResourceState[], ResourceStateAction> = (currentStates: Array<ResourceState>, action: ResourceStateAction) => {
+  if (action.type === "add") {
+    return [...currentStates, ...action.data];
+  } else if (action.type === "change") {
+    return currentStates.map(each_state => {
+      const filteredState: ResourceState[] = action.data.filter(each_data => each_data.id === each_state.id);
+      if (filteredState.length === 0) {
+        return each_state;
+      } else {
+        return filteredState[0];
+      }
+    });
+  } else if (action.type === "init") {
+    return action.data;
+  } else {
+    return [];
+  }
+}
 
 // webアプリ本体
 function App() {
   // 状態変数一覧
   // 対象の一覧変数
-  const [resources, setResources] = useReducer<Array<Resource>>(
-    (currentResource: Array<Resource>, action) => {
-      if (action.type === "add") {
-        return [...currentResource, ...action.data];
-      } else if (action.type === "change") {
-        return currentResource.map(each_resource => each_resource.id === action.data.id ? action.data : each_resource);
-      } else if (action.type === "remove") {
-        return currentResource.filter(each_resource => each_resource.id !== action.data.id);
-      } else if (action.type === "init") {
-        return action.data;
-      }
-    }, []
+  const [resources, setResources] = useReducer(
+    resourceReducer, []
   );
   // 状態の一覧変数
-  const [states, setStates] = useReducer<Array<ResourceState>>(
-    (currentStates: Array<ResourceState>, action) => {
-      if (action.type === "add") {
-        return [...currentStates, ...action.data];
-      } else if (action.type === "change") {
-        return currentStates.map(each_state => each_state.id === action.data.id ? action.data : each_state);
-      } else if (action.type === "init") {
-        return action.data;
-      }
-    }, []
+  const [states, setStates] = useReducer(
+    resourceStateReducer, []
   );
 
   // ソート制御変数
@@ -47,7 +79,7 @@ function App() {
   // エラーメッセージ表示
   const [errorMessage, setErrMessage] = useState<string>("");
   // ユーザー名用変数
-  const [userName, setUserName] = useState<string>(localStorage.getItem("user_name"));
+  const [userName, setUserName] = useState<string>(localStorage.getItem("user_name") || "");
 
   // 新しい管理対象
   const [newResourceName, setNewResourceName] = useState<string>("");
@@ -75,16 +107,16 @@ function App() {
     socket.on(SIM.OPEN_PAGE, (init_val: { data: Array<Resource>, state: Array<ResourceState> }) => {
       console.log("init page")
       console.log(init_val.data);
-      setResources({ type: "init", data: init_val.data });
+      setResources({ type: "init", data: init_val.data } as ResourceAction);
 
       console.log(init_val.state);
-      setStates({ type: "init", data: init_val.state });
+      setStates({ type: "init", data: init_val.state } as ResourceStateAction);
     });
 
     // 対象の変更通知がサーバーから来た場合
     socket.on(SIM.SERVER_SEND_CHANGED_RESOURCE, (target: Resource) => {
       console.log(`changed state: ${target.name}`);
-      setResources({ type: "change", data: target });
+      setResources({ type: "change", data: [target] } as ResourceAction);
       setDispState(true);
     });
 
@@ -92,7 +124,7 @@ function App() {
     socket.on(SIM.SERVER_ACCEPT_CHANGE, (target: Resource) => {
       setErrMessage(`${target.name}を${target.state.name}にしました`);
       console.log(`${target.name}を${target.state.name}にしました`);
-      setResources({ type: "change", data: target });
+      setResources({ type: "change", data: [target] } as ResourceAction);
       setDispState(true);
     });
 
@@ -105,26 +137,26 @@ function App() {
 
     // 状態の変更通知がサーバーから来た場合
     socket.on(SIM.SERVER_SEND_CHANGED_STATE, (target: ResourceState) => {
-      setStates({ type: "change", data: target });
+      setStates({ type: "change", data: [target] } as ResourceStateAction);
     });
 
     // 対象追加がサーバーから通知された場合
     socket.on(SIM.SERVER_ADD_RESOURCE, (resource: Resource) => {
       console.log(resources);
-      setResources({ type: "add", data: [resource] });
+      setResources({ type: "add", data: [resource] } as ResourceAction);
     });
 
     // 対象削除がサーバーから通知された場合
     socket.on(SIM.SERVER_RMV_RESOURCE, (resource: Resource) => {
       console.log(`remove ${resource.name}`);
-      setResources({ type: "remove", data: resource });
+      setResources({ type: "remove", data: [resource] } as ResourceAction);
     });
 
     // 状態追加がサーバーから通知された場合
     socket.on(SIM.SERVER_ADD_STATE, (state: ResourceState) => {
       console.log(`added state, name: ${state.name}, id: ${state.id}`);
       console.log(states);
-      setStates({ type: "add", data: [state] });
+      setStates({ type: "add", data: [state] } as ResourceStateAction);
     });
 
   }, []);
@@ -293,7 +325,7 @@ function App() {
                     states.map((state: ResourceState) => { return { label: state.name, value: state.id } })
                   }
                   onChange={(value) => {
-                    let newState: ResourceState = states.filter((state: ResourceState) => {
+                    let newState: ResourceState[] = states.filter((state: ResourceState) => {
                       return state.id === value.value;
                     });
                     setNewResourceState(newState[0]);
@@ -333,7 +365,7 @@ function App() {
         <span>状態</span>
         <select
           onChange={(e) => {
-            setNewStateType(e.target.value);
+            setNewStateType(e.target.value as StateTypes);
           }}
         >
           <option value="state">状態</option>
@@ -345,9 +377,9 @@ function App() {
           onClick={() => {
             const newState: ResourceState = {
               id: -1,
-              name: newStateName,
-              color: newStateColor,
-              type: newStateType
+              name: newStateName as string,
+              color: newStateColor as string,
+              type: newStateType as StateTypes
             }
             console.log(`add ${newState.name}`);
             socket.emit(SIM.CLIENT_ADD_STATE, newState);
